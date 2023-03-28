622a1109eac5640d6101dbb24cb90617f19e7fd9
wesselvtoorn <114135813+wesselvtoorn@users.noreply.github.com>
2023-03-28 11:14:18 +0200

    update servo gedeelte

--- main.ino
+++ main.ino
@@ -1,38 +1,37 @@
-#include <servo.h>
+#include <Servo.h>
 
 Servo servo;  
 
 void setup() {
+  bool flame = false;
   servo.attach(9);              // attaches the servo on pin 9 to the servo object
+  servo.write(90);
+
+
+  delay(1000);   //starup timer
 }
 
-bool flame(){
-  if (flame == true){
-    return true;
-  }
+// void rij(int graden, int snelheid){
 
-}
+// }
 
-void rij(int graden, int snelheid){
-
-}
-
-int draai(int snelheid, bool plus, int graden){
+int draai(int snelheid, bool plus, int begin, int end){
+    bool flame = false;
     if (plus == true){
-      for (int i = 0; i < graden; ++i){  //van 0 tot 180 graden.
+      for (int i = end; i < begin; ++i){  //van 0 tot 180 graden.
         delay(snelheid);                     //snelheid van servo, in miliseconde (1000 ms = 1 seconde)
         servo.write(i);             //hoeveel graden de servo staat.
-        if (flame() == true){
-          return graden;
+        if (flame == true){
+          return i;
         }
       } 
     }
     if (plus == false){
-      for (int i = graden; i > 0; --i){  //van 180 tot 0 graden.
+      for (int i = begin; i > end; --i){  //van 180 tot 0 graden.
         delay(snelheid);                     //snelheid van servo, in miliseconde (1000 ms = 1 seconde)
         servo.write(i);
-        if (flame() == true){
-          return graden;
+        if (flame == true){
+          return i;
         }
       }
     return 0;
@@ -41,9 +40,12 @@ int draai(int snelheid, bool plus, int graden){
 
 
 void loop() {
-  int graden = draai(15, true, 180);
-  if (graden != 0){
-    rij(graden, snelheid)
-  }
+  draai(15, false, 90, 0);
+  draai(15, true, 180, 0);
+  draai(15, false, 180, 90);
+  delay(1000);
+  // if (graden != 0){
+  //   rij(graden, snelheid)
+  // }
 
 }
