5dde419bf781493456fa46ce4fe7eea31c6db26e
wesselvtoorn <114135813+wesselvtoorn@users.noreply.github.com>
2023-04-06 11:29:53 +0200

    update main

--- main.ino
+++ main.ino
@@ -1,11 +1,17 @@
 #include <Servo.h>
+# define trigPin 6
 
-Servo servo;  
+Servo servo; 
+int flame = LOW;
+long dur, dist;
 
 void setup() {
-  bool flame = false;
+  Serial.begin(9600);
   servo.attach(9);              // attaches the servo on pin 9 to the servo object
   servo.write(90);
+  pinMode(5, INPUT);
+  pinMode(13, OUTPUT);
+  pinMode(trigPin, OUTPUT); 
 
 
   delay(1000);   //starup timer
@@ -15,35 +21,60 @@ void setup() {
 
 // }
 
+int bereken(int pin){
+    pinMode(pin, INPUT); 
+    digitalWrite(trigPin, LOW); 
+    delayMicroseconds(2); 
+    digitalWrite(trigPin, HIGH); 
+    delayMicroseconds(10); 
+    digitalWrite(trigPin, LOW); 
+    dur = pulseIn(pin, HIGH); 
+    dist = dur / 58.2; 
+    return dist;
+}
+
+
 int draai(int snelheid, bool plus, int begin, int end){
-    bool flame = false;
     if (plus == true){
       for (int i = end; i < begin; ++i){  //van 0 tot 180 graden.
         delay(snelheid);                     //snelheid van servo, in miliseconde (1000 ms = 1 seconde)
         servo.write(i);             //hoeveel graden de servo staat.
-        if (flame == true){
-          return i;
+        flame = digitalRead(5);
+        if (flame == LOW){
+          flame = HIGH;
+          return begin;
         }
       } 
-    }
-    if (plus == false){
-      for (int i = begin; i > end; --i){  //van 180 tot 0 graden.
-        delay(snelheid);                     //snelheid van servo, in miliseconde (1000 ms = 1 seconde)
-        servo.write(i);
-        if (flame == true){
-          return i;
+    } else if (plus == false){
+        for (int i = begin; i > end; --i){  //van 180 tot 0 graden.
+          delay(snelheid);                     //snelheid van servo, in miliseconde (1000 ms = 1 seconde)
+          servo.write(i);
+          flame = digitalRead(5);
+          if (flame == LOW){
+            flame = HIGH;
+            return begin;
+          }
         }
-      }
-    return 0;
   }
+  return -1;
 }
 
 
 void loop() {
-  draai(15, false, 90, 0);
-  draai(15, true, 180, 0);
-  draai(15, false, 180, 90);
-  delay(1000);
+  if (int grad = draai(15, false, 90, 0) != -1){
+    Serial.println(grad);
+    digitalWrite(13, HIGH);
+    delay(1000);
+  }else if (int grad = draai(15, true, 180, 0) != -1){
+    Serial.println(grad);
+    digitalWrite(13, HIGH);
+    delay(1000);  
+  }else if (int grad = draai(15, false, 180, 90) != -1){
+    Serial.println(grad);
+    digitalWrite(13, HIGH);
+    delay(1000);
+  }
+  digitalWrite(13, LOW);
   // if (graden != 0){
   //   rij(graden, snelheid)
   // }
