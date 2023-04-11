7c74ddd2e6d7dd5f4d8082a744375d8036eeb8bd
Tim-HU22 <113911186+Tim-HU22@users.noreply.github.com>
2023-04-11 11:29:13 +0200

    Add files via upload

--- Wielen_brandauto.ino
+++ Wielen_brandauto.ino
@@ -1,28 +1,23 @@
-int links1 = 10;
-int links2 = 11;
+int links1 = 2;
+int links2 = 4;
 int rechts1 = 12;
 int rechts2 = 13;
-int motorLeftPINPWM = 5;
-int motorRightPINPWM = 6;
 
-int hele_draai = 0;
+int hele_draai = 2250;
 
 void links(int speed) {
   
   if (speed > 0) {
     digitalWrite(links1, HIGH);
     digitalWrite(links2, LOW);
-    analogWrite(motorLeftPINPWM, speed);
   }
   else if(speed < 0){
     digitalWrite(links1, LOW);
     digitalWrite(links2, HIGH);
-    analogWrite(motorLeftPINPWM, -speed);
   }
   else {
     digitalWrite(links1, LOW);
     digitalWrite(links2, LOW);
-    analogWrite(motorLeftPINPWM, 0);
   }
 }
 
@@ -31,44 +26,53 @@ void rechts(int speed) {
   if (speed > 0) {
     digitalWrite(rechts1, HIGH);
     digitalWrite(rechts2, LOW);
-    analogWrite(motorRightPINPWM, speed);
   }
   else if(speed < 0){
     digitalWrite(rechts1, LOW);
     digitalWrite(rechts2, HIGH);
-    analogWrite(motorRightPINPWM, -speed);
   }
   else {
     digitalWrite(rechts1, LOW);
     digitalWrite(rechts2, LOW);
-    analogWrite(motorRightPINPWM, 0);
   }
 }
 
 void setup() {
   pinMode(links1, OUTPUT);
   pinMode(links2, OUTPUT);
-  pinMode(motorLeftPINPWM, OUTPUT);
   pinMode(rechts1, OUTPUT);
   pinMode(rechts2, OUTPUT);
-  pinMode(motorRightPINPWM, OUTPUT);
 }
 
 void loop() {
+  // stop
+  links(0);
+  rechts(0);
+  delay(1000);
 
   // rechtdoor
-  links(255);
-  rechts(255);
-  delay(500);
+  links(1);
+  rechts(1);
+  delay(1000);
+
+    // stop
+  links(0);
+  rechts(0);
+  delay(1000);
 
   // links
-  links(-128);
-  rechts(128);
-  delay(hele_draai * percentage_draai);
+  links(-1);
+  rechts(1);
+  delay(hele_draai*0.5);
 
-  //rechts
-  links(128);
-  rechts(-128);
-  delay(hele_draai * percentage_draai);
+  // stop
+  links(0);
+  rechts(0);
+  delay(1000);
+
+  // //rechts
+  // links(1);
+  // rechts(-1);
+  // delay(hele_draai);
   
 }
